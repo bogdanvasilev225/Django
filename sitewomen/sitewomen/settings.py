061e3c97b0dcfdaf5d80217eb0d0c876ee1e3218
Bogdan <bogdanvasilev225@gmail.com>
2024-03-05 18:15:06 +0400

    Ограничение доступа к страницам сайта с помощью декоратора login_required и класса миксина LoginRequiredMixin

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import LOGIN_REDIRECT_URL
+from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGIN_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'users:login'
