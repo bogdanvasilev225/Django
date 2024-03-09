a0705f9dec07afad0943c6e28742ee226fb42da9
Bogdan <bogdanvasilev225@gmail.com>
2024-03-09 19:29:23 +0400

    Настраил почтовый SMTP-сервер для отправки писем из фреймворка Django через яндекс-почту

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -146,4 +146,15 @@ AUTHENTICATION_BACKENDS = [
     'users.authentication.EmailAuthBackend',
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "bogdanvas1994@ya.ru"
+EMAIL_HOST_PASSWORD = "ncxcogkgfaddgscm"
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
+
